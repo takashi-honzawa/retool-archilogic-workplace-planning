b4cef463b8518d3594e38df767592474255a55d1
Takashi Honzawa <takashi@archilogic.com>
2023-07-11 17:01:49 -0400

    init

--- src/FloorPlan.js
+++ src/FloorPlan.js
@@ -1,86 +1,55 @@
 import React, { useEffect, useRef } from 'react';
 import { FloorPlanEngine } from '@archilogic/floor-plan-sdk'
 
-import { placeHub } from './helpers/placeHubs'
-import { startupSettings } from './utils/constants'
+import { startupSettings, apiBaseURL, spaceColors } from './utils/constants'
 import './FloorPlan.css'
 
 let hasLoaded = false
 let fpe
-
-let prevMaxDist
-let prevExtWallOffset
-
-let markers = []
-
-let hubCount
-let spaceProCount
-let co2MiniCount
+let spaceData
 
 const FloorPlan = ({ triggerQuery, model, modelUpdate }) => {
   const container = useRef(null);
   const { token, floorId } = model
   
   console.log('model', model)
-  
-  function addMarker(fpe, pos, className){
-    const el = document.createElement('div')
-    el.classList.add(className)
-    const marker = fpe.addHtmlMarker({
-      pos: pos,
-      el
-    })
-    return marker
-  }
-  function removeMarker(){
-    if (markers.length !== 0){
-      markers.forEach(marker => marker.remove())
-      markers = [];
-    }
-  }
 
-  function getSensors(fpe){
-    const spaces = fpe.resources.spaces
-    const selectedSpaces = spaces.filter(space => {
-      return space.program !== 'void' && space.program !== 'circulate' && space.program !== 'care' && space.usage !== 'undefined'
-    })
-    let miniCount = 0
-    let proCount = 0
-    selectedSpaces.forEach(space => {
-      if(space.area < (100 / 10.764)){
-        const marker = addMarker(fpe, [space.center[0], space.center[1]], 'co2-marker')
-        markers.push(marker)
-        miniCount += 1
-      } else {
-        const marker = addMarker(fpe, [space.center[0], space.center[1]], 'air-marker')
-        markers.push(marker)
-        proCount += 1
+  function colorCodeSpaces(spaces){
+    spaceData.features.forEach(data => {
+      if(data.properties.customAttributes.length === 0) return
+            
+      const match = spaces.find(space => space.id === data.id)
+      const zoneType = data.properties.customAttributes.find(customAttribute => customAttribute.apiFieldName === 'zoneType')
+    
+      if(zoneType.value === 'focus'){
+        match.node.setHighlight({
+          fill: spaceColors.focus,
+          fillOpacity: 1
+        })
+      }else if(zoneType.value === 'collaboration'){
+        match.node.setHighlight({
+          fill: spaceColors.collaboration,
+          fillOpacity: 1
+        })
+      }else if(zoneType.value === 'recharge'){
+        match.node.setHighlight({
+          fill: spaceColors.recharge,
+          fillOpacity: 1
+        })
       }
     })
-    spaceProCount = proCount
-    co2MiniCount = miniCount
   }
-  function getHubs(fpe, maxDist, extWallOffset){
-    const maxHubDistanceInFt = maxDist !== 0 ? maxDist : 70
-    const wallOffsetInFt = extWallOffset !== 0 ? extWallOffset : 3
-
-    const hubs = placeHub(fpe, maxHubDistanceInFt, wallOffsetInFt)
-    hubCount = hubs.length
-    hubs.forEach(hub => {
-      const marker = addMarker(fpe, hub.point, 'hub-marker')
-      markers.push(marker)
-    })
-    
-    prevMaxDist = maxHubDistanceInFt
-    prevExtWallOffset = wallOffsetInFt
-  }
-  
   async function initFloorPlan(){
     if(!token || !floorId) return
     fpe = new FloorPlanEngine(container.current, startupSettings)
-      await fpe.loadScene(floorId, {publishableToken: token})
-      hasLoaded = floorId
-      return fpe
+    await fpe.loadScene(floorId, {publishableToken: token})
+    hasLoaded = floorId
+
+    spaceData = await fetch(
+      `${apiBaseURL}space?floorId=${floorId}&includeCustomFields=true&includeCustomAttributes=true&pubtoken=${token}`
+    ).then((response) => response.json())
+
+    return fpe
   }
   
   useEffect(() => {
@@ -88,21 +57,9 @@ const FloorPlan = ({ triggerQuery, model, modelUpdate }) => {
     if(container.current){
       initFloorPlan()
       .then((fpe) => {
-        if(!fpe) return
-        getHubs(fpe, model.maxDist, model.extWallOffset)
-        getSensors(fpe)
-        modelUpdate({hubCount, spaceProCount, co2MiniCount})
+        colorCodeSpaces(fpe.resources.spaces)
       })
     }
-  })
-
-  useEffect(() => {
-    if(!hasLoaded) return
-    if(model.maxDist === prevMaxDist && model.extWallOffset === prevExtWallOffset) return
-    removeMarker()
-    getHubs(fpe, model.maxDist, model.extWallOffset)
-    getSensors(fpe)
-    modelUpdate({hubCount, spaceProCount, co2MiniCount})
   })
   
   return(
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,3 +17,11 @@ export const startupSettings = {
     system: "imperial"
   }
 }
+
+export const apiBaseURL = 'https://api.archilogic.com/v2/'
+
+export const spaceColors = {
+  focus: [200, 225, 241], //"#c8e1f1"
+  collaboration: [252, 228, 226], //"#fce4e2"
+  recharge: [218, 231, 210],//"#dae7d2"
+}
